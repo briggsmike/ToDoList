6567df3a2473f8838e6121f084a8bd42c6e358be
Briggs <54554227+briggsmike@users.noreply.github.com>
2022-10-27 17:43:24 -0500

    fixed delete button

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-document.getElementById("addBtn").addEventListener("click", newElement);
+//document.getElementById("addBtn").addEventListener("click", newElement);
+document.getElementById("myForm").addEventListener("submit", newElement);
+ 
 
 
 // Create a new list item when clicking on the "Add" button
-function newElement() {
+function newElement(event) {
+    event.preventDefault();
   var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
   var t = document.createTextNode(inputValue);
@@ -18,10 +21,11 @@ function newElement() {
     let xBtn = document.createElement("button");
     let txt = document.createTextNode("\u00D7");
     xBtn.className = "closeBtn";
+    xBtn.addEventListener("click", removeElement);
     xBtn.appendChild(txt);
     li.appendChild(xBtn);  
 }
 
 function removeElement(elem) {
-
+    elem.target.parentNode.remove();
 }
