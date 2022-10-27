58a4f007e05746ded17eda8f3ca62d8e5765a0d0
Briggs <54554227+briggsmike@users.noreply.github.com>
2022-10-27 15:35:26 -0500

    double checking

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 document.getElementById("addBtn").addEventListener("click", newElement);
 
+
 // Create a new list item when clicking on the "Add" button
 function newElement() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') {
-      alert("You gotta write something!");
-    } else {
-      document.getElementById("myUL").appendChild(li);
-    }
-    document.getElementById("myInput").value = "";
-  
-  
-      let xBtn = document.createElement("button");
-      let txt = document.createTextNode("\u00D7");
-      xBtn.className = "closeBtn";
-      xBtn.appendChild(txt);
-      li.appendChild(xBtn);  
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You gotta write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
   }
-// remove an element when X is clicked
-  function removeElement(elem) {
-  
-  }
-  
+  document.getElementById("myInput").value = "";
 
+
+    let xBtn = document.createElement("button");
+    let txt = document.createTextNode("\u00D7");
+    xBtn.className = "closeBtn";
+    xBtn.appendChild(txt);
+    li.appendChild(xBtn);  
+}
+
+function removeElement(elem) {
+
+}
