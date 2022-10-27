2d921baabbaf269a4698be414a36d68b2c04b291
Briggs <54554227+briggsmike@users.noreply.github.com>
2022-10-27 15:31:28 -0500

    added delete button to list elements

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
+document.getElementById("addBtn").addEventListener("click", newElement);
 
-
-
-const list = document.getElementById("todoList");
-const form = document.getElementById("todoForm");
-
+// Create a new list item when clicking on the "Add" button
 function newElement() {
-    const li = document.createElement("li");
-    const inputValue = document.getElementById("inputForm").value;
-    const t = document.createTextNode(inputValue);
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
     li.appendChild(t);
-    if (inputValue === "") {
-        alert("You gotta write something!");
+    if (inputValue === '') {
+      alert("You gotta write something!");
     } else {
-        document.getElementById("todoList").appendChild(li);
+      document.getElementById("myUL").appendChild(li);
     }
-    document.getElementById("inputForm") = "";
-}
+    document.getElementById("myInput").value = "";
+  
+  
+      let xBtn = document.createElement("button");
+      let txt = document.createTextNode("\u00D7");
+      xBtn.className = "closeBtn";
+      xBtn.appendChild(txt);
+      li.appendChild(xBtn);  
+  }
+// remove an element when X is clicked
+  function removeElement(elem) {
+  
+  }
+  
 
-function launch() {
-    form.addEventListener("submit", newElement);
-}
-launch();
